5b260a35bcf83e206b5de2d6a829a771fc7c7bdd
Rayca Thais <raycathaisbn@gmail.com>
2025-04-21 15:24:33 -0300

    Adiciona Avaliacao.java da main para a branch

---
+++ src/org/serratec/trabalho/modelos/Avaliacao.java
@@ -0,0 +1,34 @@
+package org.serratec.trabalho.modelos;
+
+import java.time.LocalDate;
+
+class Avaliacao {
+    private Aluno aluno;
+    private LocalDate data;
+    private Personal personal;
+    private String descricao;
+
+    public Avaliacao(Aluno aluno, LocalDate data, Personal personal, String descricao) {
+        this.aluno = aluno;
+        this.data = data;
+        this.personal = personal;
+        this.descricao = descricao;
+    }
+
+    public LocalDate getData() {
+        return data;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public Aluno getAluno() {
+        return aluno;
+    }
+
+    public Personal getPersonal() {
+        return personal;
+    }
+}
+
